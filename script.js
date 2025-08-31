1d26b18a632cb8ccf651cf0611aab5854c0c6945
Yoshiro Odaki <156771093+yossshhi@users.noreply.github.com>
2025-08-31 19:46:59 +1000

    Add computer generated choice for game

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+// Score Count
+const humanScore = 0;
+const computerScore = 0;
+
+
+function getComputerChoice() {
+    const minCeiled = Math.ceil(0); // 0 inclusive
+    const maxFLoored = Math.floor(2); // 2 inclusive 
+    let computerChoice = Math.floor(Math.random() * (maxFLoored - minCeiled + 1) + minCeiled);
+
+    if (computerChoice == 0){
+        return "Rock"
+    } else if (computerChoice == 1) {
+        return "Paper"
+    } else if (computerChoice == 2){
+        return "Scissors"
+    }
+}
+
